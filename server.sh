9489fd5ded6b08a937ede6997fcd3624fe18b07a
vnys-codecamp <6330338+vnys-codecamp@users.noreply.github.com>
2023-08-05 20:39:03 +0000

    update config

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-browser-sync start --server '.' --files '*.html, *.css, *.js' --port 3000 --no-ui --no-open &
+browser-sync start --server '.' --files '*.html, *.css, *.js' --port 3000 --no-ui --no-open
