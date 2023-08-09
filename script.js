ac07f5045f1509076708373f2c735e6fbfef2a48
Maja Jaakson <maja@jaakson.ca>
2023-08-09 14:39:41 +0000

    Add onclick challenge

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//! todo: do something in this file
+/* 
+Oh, thank goodness you're here 🙏 As you can see below, I created a function
+called myOnClickFunction. It is currently called whenever the "onclick" event fires
+for the button in our button.html file.
+
+I defined another two functions for use in the card component, but I didn't manage to
+add onclick functions to the buttons in card.html... and I couldn't think of good 
+messages to display when those buttons are clicked. PLEASE HELP ME OUT 😭
+*/
+
+const myOnClickFunction = () => {
+  alert("Your button was clicked and called a function in script.js!");
+};
+const myOkClickFunction = () => {
+  // what can we display for the user when OK is clicked?...
+};
+const myCancelClickFunction = () => {
+  // what can we display for the user when Cancel is clicked?...
+};
